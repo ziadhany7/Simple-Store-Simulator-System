2e6d2f69ffafe5f0d41fe9d41a6d5eb1a3b41fb2
Tasnim Ahmed <145922166+tasnim2025@users.noreply.github.com>
2024-12-05 12:34:57 +0200

    Update cart.fs

--- cart.fs
+++ cart.fs
@@ -1,1 +1,16 @@
 ﻿module Cart
+open Product
+
+type Cart = { Items: list<Product> }
+
+let emptyCart = { Items = [] }
+
+let addToCart product cart =
+    { cart with Items = product :: cart.Items }
+
+let removeFromCart productName cart =
+    let updatedItems = cart.Items |> List.filter (fun p -> p.Name <> productName)
+    { cart with Items = updatedItems }
+
+let calculateTotal cart =
+    cart.Items |> List.sumBy (fun p -> p.Price) // F# uses float addition here
